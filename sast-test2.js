21a944e21ad4703e6a9307fd81ef1c3e46fd0203
Dushan Wijesinghe <dushan566@gmail.com>
2024-07-18 21:50:15 +0100

    SQL Injection

--- sast-test2.js
+++ sast-test2.js
@@ -1,2 +1,12 @@
-txtUserId = getRequestString("UserId");
-txtSQL = "SELECT * FROM Users WHERE UserId = " + txtUserId;
+@app.route("/login")
+def login():
+
+  username = request.values.get('username')
+  password = request.values.get('password')
+
+  # Prepare database connection
+  db = pymysql.connect("localhost")
+  cursor = db.cursor()
+
+  # Execute the vulnerable SQL query concatenating user-provided input.
+  cursor.execute("SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (username, password))
